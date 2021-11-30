afcda5e5abdfc969c469228543fc5f59f4dfd893
Carlos Peraza <carlosperaza17@gmail.com>
2021-11-29 20:42:31 -0600

    test layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -61,7 +61,7 @@ const Layout = props => {
                 <link rel="stylesheet" href="css/style.css" />
             </Head>  
 
-            <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span>
+            {/* <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span>
             <aside id="colorlib-aside" role="complementary" className="js-fullheight text-center">
             <Link href="/"><h1 id="colorlib-logo" className="cursor-pointer">Carlos</h1></Link>              
                 <nav id="colorlib-main-menu" role="navigation">
@@ -90,7 +90,7 @@ const Layout = props => {
                     Copyright &copy;{new Date().getFullYear()}
                     
                 </div>
-            </aside>
+            </aside> */}
 
             <div id="colorlib-main">
                 <main>
