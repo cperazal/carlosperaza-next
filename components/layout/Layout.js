6cd35f027313dc9f41d2fe38c6472d96dff6af7b
Carlos Peraza <carlosperaza17@gmail.com>
2021-11-30 20:56:38 -0600

    test layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,37 +5,37 @@ import $ from 'jquery';
 
 const Layout = props => {
 
-    useEffect(() => {
-        $(document).click(function (e) {
-            var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
+    // useEffect(() => {
+    //     $(document).click(function (e) {
+    //         var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+    //         if (!container.is(e.target) && container.has(e.target).length === 0) {
         
-                if ( $('body').hasClass('offcanvas') ) {
+    //             if ( $('body').hasClass('offcanvas') ) {
         
-                    $('body').removeClass('offcanvas');
-                    $('.js-colorlib-nav-toggle').removeClass('active');
+    //                 $('body').removeClass('offcanvas');
+    //                 $('.js-colorlib-nav-toggle').removeClass('active');
                 
-                }
+    //             }
                 
-            }
-        });
+    //         }
+    //     });
 
-        if ($('body').hasClass('offcanvas')) {
-            document.querySelector("#btnBurger").classList.remove("active");
-            $('body').removeClass('offcanvas');	
-        }
+    //     if ($('body').hasClass('offcanvas')) {
+    //         document.querySelector("#btnBurger").classList.remove("active");
+    //         $('body').removeClass('offcanvas');	
+    //     }
 
-    })
+    // })
     
-    const onClickBurger = () => {
-        if ($('body').hasClass('offcanvas')) {
-            document.querySelector("#btnBurger").classList.remove("active");
-            $('body').removeClass('offcanvas');	
-        } else {
-            document.querySelector("#btnBurger").classList.add("active");
-            $('body').addClass('offcanvas');	
-        }
-    }
+    // const onClickBurger = () => {
+    //     if ($('body').hasClass('offcanvas')) {
+    //         document.querySelector("#btnBurger").classList.remove("active");
+    //         $('body').removeClass('offcanvas');	
+    //     } else {
+    //         document.querySelector("#btnBurger").classList.add("active");
+    //         $('body').addClass('offcanvas');	
+    //     }
+    // }
 
     return ( 
         <>
@@ -61,7 +61,7 @@ const Layout = props => {
                 <link rel="stylesheet" href="css/style.css" />
             </Head>  
 
-            <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span>
+            {/* <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span> */}
             <aside id="colorlib-aside" role="complementary" className="js-fullheight text-center">
             <Link href="/"><h1 id="colorlib-logo" className="cursor-pointer">Carlos</h1></Link>              
                 <nav id="colorlib-main-menu" role="navigation">
