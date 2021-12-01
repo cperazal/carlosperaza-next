3b69a126e6f7ef577c0d351be316a06d88d0869f
Carlos Peraza <carlosperaza17@gmail.com>
2021-11-30 21:04:30 -0600

    revert changes & quit link css

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,41 +5,41 @@ import $ from 'jquery';
 
 const Layout = props => {
 
-    // useEffect(() => {
-    //     $(document).click(function (e) {
-    //         var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
-    //         if (!container.is(e.target) && container.has(e.target).length === 0) {
+    useEffect(() => {
+        $(document).click(function (e) {
+            var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
         
-    //             if ( $('body').hasClass('offcanvas') ) {
+                if ( $('body').hasClass('offcanvas') ) {
         
-    //                 $('body').removeClass('offcanvas');
-    //                 $('.js-colorlib-nav-toggle').removeClass('active');
+                    $('body').removeClass('offcanvas');
+                    $('.js-colorlib-nav-toggle').removeClass('active');
                 
-    //             }
+                }
                 
-    //         }
-    //     });
+            }
+        });
 
-    //     if ($('body').hasClass('offcanvas')) {
-    //         document.querySelector("#btnBurger").classList.remove("active");
-    //         $('body').removeClass('offcanvas');	
-    //     }
+        if ($('body').hasClass('offcanvas')) {
+            document.querySelector("#btnBurger").classList.remove("active");
+            $('body').removeClass('offcanvas');	
+        }
 
-    // })
+    })
     
-    // const onClickBurger = () => {
-    //     if ($('body').hasClass('offcanvas')) {
-    //         document.querySelector("#btnBurger").classList.remove("active");
-    //         $('body').removeClass('offcanvas');	
-    //     } else {
-    //         document.querySelector("#btnBurger").classList.add("active");
-    //         $('body').addClass('offcanvas');	
-    //     }
-    // }
+    const onClickBurger = () => {
+        if ($('body').hasClass('offcanvas')) {
+            document.querySelector("#btnBurger").classList.remove("active");
+            $('body').removeClass('offcanvas');	
+        } else {
+            document.querySelector("#btnBurger").classList.add("active");
+            $('body').addClass('offcanvas');	
+        }
+    }
 
     return ( 
         <>
-            {/* <Head>
+            <Head>
                 <title>{process.env.NEXT_PUBLIC_PAGE_TITLE}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="robots" content= "index, follow"/>	
@@ -56,13 +56,13 @@ const Layout = props => {
                 <meta property="og:description" content="Hola, mi nombre es Carlos Peraza soy ingeniero en informatica y desarrollador de software." />
                 <meta name="msapplication-tooltip" content="Hola, mi nombre es Carlos Peraza soy ingeniero en informatica y desarrollador de software." />       
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" />
+                {/* <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet" />
-                <link rel="stylesheet" href="css/style.css" />
-            </Head>   */}
+                <link rel="stylesheet" href="css/style.css" /> */}
+            </Head>  
 
-            {/* <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span> */}
-            {/* <aside id="colorlib-aside" role="complementary" className="js-fullheight text-center">
+            <span onClick={() => onClickBurger()} className="js-colorlib-nav-toggle colorlib-nav-toggle" id="btnBurger"><i></i></span>
+            <aside id="colorlib-aside" role="complementary" className="js-fullheight text-center">
             <Link href="/"><h1 id="colorlib-logo" className="cursor-pointer">Carlos</h1></Link>              
                 <nav id="colorlib-main-menu" role="navigation">
                     <ul>
@@ -90,7 +90,7 @@ const Layout = props => {
                     Copyright &copy;{new Date().getFullYear()}
                     
                 </div>
-            </aside> */}
+            </aside>
 
             <div id="colorlib-main">
                 <main>
